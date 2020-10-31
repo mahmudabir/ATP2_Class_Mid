2cfa4f849d072702c609c99ea51630936e7c9500
Abir Mahmud <amabirmahmud@gmail.com>
2020-10-31 13:05:22 +0600

    Data passed by using model binding

--- Data Passing/Controllers/HomeController.cs
+++ Data Passing/Controllers/HomeController.cs
@@ -11,68 +11,29 @@ namespace Data_Passing.Controllers
     {
         // GET: Home
 
-        //Session
-        //ViewData
-        //ViewBag
-        //TempData
-        //Model: ViewModel, DataModel
-
-        //public ActionResult Index()
-        //{
-        //    if (Request.HttpMethod=="GET")
-        //    {
-        //        return View();
-        //    }
-        //    else
-        //    {
-        //        return Content("<h2>It works</h2>");
-        //    }
-
-        //    //return View();
-        //}
-
-
+        [HttpGet]
         public ActionResult Index()
         {
-            if (Request.HttpMethod == "GET")
-            {
-                return View();
-            }
-            else
-            {
-                TempData["name"] = Request["name"];
-                TempData["username"] = Request["username"];
-                TempData["blood_group"] = Request["blood_group"];
-                TempData["gender"] = Request["gender"];
-                TempData["dob"] = Request["dob"];
-            }
+             return View();
+        }
 
+
+        [HttpPost]
+        public ActionResult Index(Person p, string confirm_password)
+        {
+
+            TempData["person"] = p;
             return RedirectToAction("Another");
         }
 
 
+        [HttpGet]
         public ActionResult Another()
         {
-            Person p = new Person { name = "Raiyan Rafi", emails = "rafi@gmail.com" };
-
-            // accessible throughout the whole session
-            //Session["mname"] = "Abir Mahmud";
-
-            // accessible for 1st successful view response but after returning once tempdata is unavailable
-            //TempData["oname"] = "Abdul Alim";//Can assign through another model to here when redirected to this controller
-
-            //Just like viewbad but uses associative array concept & also accessible for one response
-            //ViewData["fname"] = "Abir";
-
-            //accessible from the view from which the viewbag was assigned & follows the object oriented concept & also accessible for one response
-            //ViewBag.lname = "Mahmud";
-
-            //ViewModel used Passing Value controller to view
-
-
-            //DataModel used for data related operations
-
-            return View();
+            Person p = TempData["Person"] as Person;
+            return View(p);
         }
+
+
     }
 }
--- Data Passing/Models/Person.cs
+++ Data Passing/Models/Person.cs
@@ -7,7 +7,12 @@ namespace Data_Passing.Models
 {
     public class Person
     {
-        public string name { get; set; }
-        public string emails { get; internal set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; internal set; }
+        public string Password { get; set; }
+        public string BloodGroup { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
     }
 }
